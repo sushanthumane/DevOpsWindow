7a10e8614528536967306cc3543c705cbe9dc72e
sushanthumane <96003928+sushanthumane@users.noreply.github.com>
2022-07-20 14:26:29 +0530

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+       
+         SqlConnection con = new SqlConnection(@"data source = RENTAL-5CG8245L; initial catalog=EmployeeDB ;uid=sa;password=Password123");
+         SqlCommand cmd=new SqlCommand();
+        SqlDataReader dr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+     }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd.CommandText = "Select * from Employee";
+            cmd.Connection = con;
+            dr = cmd.ExecuteReader();
+            while(dr.Read())
+            {
+                ListViewItem lv = new ListViewItem(dr[0].ToString());
+                lv.SubItems.Add(dr[1].ToString());
+                lv.SubItems.Add(dr[2].ToString());
+                lv.SubItems.Add(dr[3].ToString());
+                lv.SubItems.Add(dr[4].ToString());
+                lv.SubItems.Add(dr[5].ToString());
+                listView1.Items.Add(lv);
+            }
+            con.Close(); 
+
+        }
+    }
+}
